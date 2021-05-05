6a9b7fa72233836675cad78c0c1ee7540133bd48
Nelson Loyola <nloyola@gmail.com>
2021-05-05 11:05:52 +0200

    Got tired of entering these commands one by one

---
+++ clinic-locator-dev.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source ~/src/nelson/python/clinic-locator/django/venv/bin/activate
+
+cd ~/src/nelson/python/clinic-locator
+emacs &
+
+cd ~/src/nelson/python/clinic-locator/django/cliniclocator
---
+++ locator-client-dev.sh
@@ -0,0 +1,4 @@
+#~/bin/bash
+
+cd ~/src/nelson/js/locator-client
+alacritty --title "angular" --option font.size=10 -e zsh -c "npm run start; zsh"
