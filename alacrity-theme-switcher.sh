16eea291d49910dc8e0e09c8c7dbac971015afaf
Nelson Loyola <nloyola@gmail.com>
2025-04-25 09:08:30 +0200

    script to change the alacrity theme

---
+++ alacrity-theme-switcher.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+THEME_DIR="$HOME/.config/alacritty/themes/themes"
+CONFIG_FILE="$HOME/.config/alacritty/alacritty.yml"
+
+# Let the user pick a theme using rofi
+THEME=$(ls "$THEME_DIR" | rofi -dmenu -p "Select Alacritty Theme")
+
+# Exit if no theme selected
+[ -z "$THEME" ] && exit 1
+
+alacritty msg config "$(cat $THEME_DIR/$THEME)"
