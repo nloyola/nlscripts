0a6b75b2e8d705c97a21facba3b7e20569418722
Nelson Loyola <nloyola@gmail.com>
2026-03-09 09:50:51 +0100

    Update ncspot build script and add keyboard fix helper

---
+++ fix_kbd.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# 1. Clear existing mappings
+setxkbmap -option ""
+# 2. Force the swap and set F-keys to standard mode
+setxkbmap -option "altwin:swap_alt_win"
+echo 2 | sudo tee /sys/module/hid_apple/parameters/fnmode
+
